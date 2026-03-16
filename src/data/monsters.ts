2d71c7889ed60cd13e9e1451f04c6292b9da47c2
Milan Kněz <milan.knez@abra.eu>
2026-03-16 09:39:44 +0100

    Monster update

--- src/data/monsters.ts
+++ src/data/monsters.ts
@@ -1,4 +1,4 @@
-import monster001 from './monsters/001.json';
+ import monster001 from './monsters/001.json';
 import monster002 from './monsters/002.json';
 import monster003 from './monsters/003.json';
 import monster004 from './monsters/004.json';
@@ -13,8 +13,34 @@ import monster012 from './monsters/012.json';
 import monster013 from './monsters/013.json';
 import monster014 from './monsters/014.json';
 import monster015 from './monsters/015.json';
+import monster016 from './monsters/016.json';
+import monster017 from './monsters/017.json';
+import monster018 from './monsters/018.json';
+import monster019 from './monsters/019.json';
+import monster020 from './monsters/020.json';
+import monster021 from './monsters/021.json';
+import monster022 from './monsters/022.json';
+import monster023 from './monsters/023.json';
+import monster024 from './monsters/024.json';
+import monster025 from './monsters/025.json';
+import monster026 from './monsters/026.json';
+import monster027 from './monsters/027.json';
+import monster028 from './monsters/028.json';
 
 export const monsterDB = [
+  monster028,
+  monster027,
+  monster026,
+  monster025,
+  monster024,
+  monster023,
+  monster022,
+  monster021,
+  monster020,
+  monster019,
+  monster018,
+  monster017,
+  monster016,
   monster015,
   monster001,
   monster002,
